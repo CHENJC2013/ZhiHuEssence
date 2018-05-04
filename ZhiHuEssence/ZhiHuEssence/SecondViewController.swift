2c41fd50a01c51472c03bfcea65aca5d5db771f3
CHENJC <chenjicai@rrjc.com>
2018-05-04 16:20:03 +0800

    项目初始化

---
+++ ZhiHuEssence/ZhiHuEssence/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ZhiHuEssence
+//
+//  Created by CHENJC on 2018/5/4.
+//  Copyright © 2018年 JC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
